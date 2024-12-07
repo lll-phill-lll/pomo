0b16b89dd6493716a631de3d0c848688347f3a07
Mikhail Filitov <filitovme@gmail.com>
2024-12-07 11:35:21 +0300

    smth

--- src/main.rs
+++ src/main.rs
@@ -83,26 +83,27 @@ const DIGITS: [&[&str]; 10] = [
     ],
 ];
 
+const TOMATO_TEMPLATE : [&str; 13]= [
+    "           ██     █       ",
+    "        ███ ██  ██        ",
+    "    ████░░██░███░░████    ",
+    "  ██░░░░░█░░██░░░██░░░██  ",
+    "██░░░░░░░░░█░░░░░░░█░░░░██",
+    "██░░░░░░░░░░░░░░░░░░░░░░██",
+    "██░░░░░░░░░░░░░░░░░░░░░░██",
+    "██░░░░░░░░░░░░░░░░░░░░░░██",
+    "██░░░██░░░░░░░░░░░░░░░░░██",
+    "  ██░░░░██░░░░░░░░░░░░██  ",
+    "    ██░░░░░░░░░░░░░░██    ",
+    "      ██████████████      ",
+    "                          ",
+];
+
 fn draw_tomato(percentage: f32) {
-    let tomato_template = [
-        "           ██     █       ",
-        "        ███ ██  ██        ",
-        "    ████░░██░███░░████    ",
-        "  ██░░░░░█░░██░░░██░░░██  ",
-        "██░░░░░░░░░█░░░░░░░█░░░░██",
-        "██░░░░░░░░░░░░░░░░░░░░░░██",
-        "██░░░░░░░░░░░░░░░░░░░░░░██",
-        "██░░░░░░░░░░░░░░░░░░░░░░██",
-        "██░░░██░░░░░░░░░░░░░░░░░██",
-        "  ██░░░░██░░░░░░░░░░░░██  ",
-        "    ██░░░░░░░░░░░░░░██    ",
-        "      ██████████████      ",
-        "                          ",
-    ];
 
-    let filled_lines = (percentage * tomato_template.len() as f32).round() as usize;
+    let filled_lines = (percentage * TOMATO_TEMPLATE.len() as f32).round() as usize;
 
-    for (i, line) in tomato_template.iter().enumerate() {
+    for (i, line) in TOMATO_TEMPLATE.iter().enumerate() {
         if i < filled_lines {
             stdout()
                 .execute(SetForegroundColor(Color::Green))
@@ -139,7 +140,7 @@ fn draw_graphic_timer(minutes: u64, seconds: u64) {
         print!(" ");
         for part in &timer_graphic {
             print!("{}", part[row]);
-            print!("  "); // Отступ между цифрами
+            print!("  ");
         }
         println!();
     }
@@ -153,31 +154,26 @@ fn main() {
     loop {
         let percentage = elapsed_seconds as f32 / total_seconds as f32;
 
-        // Очистка экрана
         stdout()
             .execute(Clear(ClearType::All))
             .unwrap()
             .execute(cursor::MoveTo(0, 0))
             .unwrap();
 
-        // Рисуем помидор и таймер
         draw_tomato(percentage);
         draw_graphic_timer(
             (total_seconds - elapsed_seconds) / 60,
             (total_seconds - elapsed_seconds) % 60,
         );
 
-        // Проверяем завершение таймера
         if elapsed_seconds >= total_seconds {
             break;
         }
 
-        // Ждём одну секунду
         thread::sleep(Duration::from_secs(1));
         elapsed_seconds += 1;
     }
 
-    // Завершающее сообщение
     stdout()
         .execute(Print("Pomodoro завершён!\n"))
         .unwrap();
